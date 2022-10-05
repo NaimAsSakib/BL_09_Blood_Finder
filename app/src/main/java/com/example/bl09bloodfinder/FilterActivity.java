118e2bcc084978582be4e22bd20b76b6af548036
NaimAsSakib <ishtiaque.morshed@sslwireless.com>
2022-10-05 20:07:51 +0600

    Took fragments for FilterActivity.java

--- app/src/main/java/com/example/bl09bloodfinder/FilterActivity.java
+++ app/src/main/java/com/example/bl09bloodfinder/FilterActivity.java
@@ -1,15 +1,26 @@
 package com.example.bl09bloodfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class FilterActivity extends AppCompatActivity {
+
+    CardView cardViewName,cardViewBlood, cardViewLocation;
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        cardViewName=findViewById(R.id.cardView1);
+        cardViewBlood=findViewById(R.id.cardView2);
+        cardViewLocation=findViewById(R.id.cardView3);
+        button=findViewById(R.id.btnFilterApply);
 
 
     }
---
+++ app/src/main/java/com/example/bl09bloodfinder/FragFilterBlood.java
@@ -0,0 +1,26 @@
+package com.example.bl09bloodfinder;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragFilterBlood extends Fragment {
+
+
+    public FragFilterBlood() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag_filter_blood, container, false);
+    }
+}
