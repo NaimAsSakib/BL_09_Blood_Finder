c498826d63fde114842a996ae14da52644301c0f
NaimAsSakib <ishtiaque.morshed@sslwireless.com>
2022-10-08 21:05:25 +0600

    Added listener for FragFilterName.java to pass input name to activity.

--- app/src/main/java/com/example/bl09bloodfinder/FilterActivity.java
+++ app/src/main/java/com/example/bl09bloodfinder/FilterActivity.java
@@ -8,15 +8,21 @@ import androidx.fragment.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.database.Query;
 
-public class FilterActivity extends AppCompatActivity {
+public class FilterActivity extends AppCompatActivity implements ItemOnClickListener{
 
     CardView cardViewName, cardViewBlood, cardViewLocation;
 
+    Button searchButton;
+
+    ItemOnClickListener itemOnClickListener;
+
+    String searchByName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +33,14 @@ public class FilterActivity extends AppCompatActivity {
         cardViewBlood = findViewById(R.id.cardView2);
         cardViewLocation = findViewById(R.id.cardView3);
 
+        searchButton=findViewById(R.id.btnFilterApply);
+
 
 
         cardViewName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openFragment(new FragFilterName());
+                openFragment(new FragFilterName(FilterActivity.this));
             }
         });
 
@@ -56,5 +64,13 @@ public class FilterActivity extends AppCompatActivity {
     //method for opening fragments
     private void openFragment(Fragment fragment) {
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).addToBackStack(null).commit();
+    }
+
+
+    @Override
+    public void onItemClicked(String value, String name) {
+        searchByName=value;
+        Log.e("Name is ",searchByName);
+
     }
 }
--- app/src/main/java/com/example/bl09bloodfinder/FragFilterName.java
+++ app/src/main/java/com/example/bl09bloodfinder/FragFilterName.java
@@ -1,5 +1,6 @@
 package com.example.bl09bloodfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class FragFilterName extends Fragment {
 
+    EditText nameInput;
+    Button button;
+
+    String name;
+    private ItemOnClickListener listener;
 
 
-    public FragFilterName() {
+    public FragFilterName(ItemOnClickListener listener) {
         // Required empty public constructor
+        this.listener=  listener;
     }
 
 
@@ -23,6 +33,26 @@ public class FragFilterName extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_frag_filter_name, container, false);
+
+        nameInput=view.findViewById(R.id.etDonorName);
+        button=view.findViewById(R.id.btnFragName);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name=nameInput.getText().toString().trim();
+
+                if (name.isEmpty()){
+                    Toast.makeText(getActivity(),"Please enter a name",Toast.LENGTH_LONG).show();
+                }else {
+                    listener.onItemClicked(name,"FragName");
+                    Intent intent = new Intent(getActivity(), FilterActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
         return  view;
 
     }
